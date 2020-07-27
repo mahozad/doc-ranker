6f7ec1a1466455e806c597979098ece5a9fb261b
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2020-07-27 18:41:41 +0430

    Add constructor and update some fields for Attribute & Doc classes

--- code/searchia-ranking/src/Ranker.java
+++ code/searchia-ranking/src/Ranker.java
@@ -1,4 +1,6 @@
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 public class Ranker {
 
@@ -11,16 +13,16 @@ public class Ranker {
             int limit) {
 
         return List.of(
-                new Doc(1),
-                new Doc(2),
-                new Doc(3),
-                new Doc(4),
-                new Doc(5),
-                new Doc(6),
-                new Doc(7),
-                new Doc(8),
-                new Doc(9),
-                new Doc(10)
+                new Doc(1, null, 0, Collections.emptyList()),
+                new Doc(2, null, 0, Collections.emptyList()),
+                new Doc(3, null, 0, Collections.emptyList()),
+                new Doc(4, null, 0, Collections.emptyList()),
+                new Doc(5, null, 0, Collections.emptyList()),
+                new Doc(6, null, 0, Collections.emptyList()),
+                new Doc(7, null, 0, Collections.emptyList()),
+                new Doc(8, null, 0, Collections.emptyList()),
+                new Doc(9, null, 0, Collections.emptyList()),
+                new Doc(10, null, 0, Collections.emptyList())
         );
     }
 }
@@ -47,10 +49,14 @@ class Doc {
 
     private int id;
     private double score;
+    private Map<String, ?> customAttrs;
     private List<Attribute<String>> searchableAttrs;
 
-    public Doc(int id) {
+    public Doc(int id, Map<String, ?> customAttrs, double score, List<Attribute<String>> searchableAttrs) {
         this.id = id;
+        this.customAttrs = customAttrs;
+        this.score = score;
+        this.searchableAttrs = searchableAttrs;
     }
 
     public int getId() {
@@ -62,6 +68,27 @@ class Attribute<T> {
 
     private String name;
     private T value;
+
+    public Attribute(String name, T value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
 }
 
 class Promotion {
