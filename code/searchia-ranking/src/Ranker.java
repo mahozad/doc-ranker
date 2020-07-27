f34bdaedce4487b693b3d6db137a372eba16cf17
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2020-07-27 12:06:14 +0430

    Initial commit

---
+++ code/searchia-ranking/src/Ranker.java
@@ -0,0 +1,40 @@
+import java.util.List;
+
+public class Ranker {
+
+    public static List<Doc> rank(
+            String query,
+            List<Doc> matches,
+            List<Promotion> promotions,
+            RankConfiguration configuration,
+            int offset,
+            int limit) {
+
+    }
+}
+
+class RankConfiguration {
+
+    private Attribute sort;
+    private double[] geoLocation;
+}
+
+class Doc {
+
+    private int id;
+    private double score;
+    private List<Attribute> retrievedAttrs;
+    private List<Attribute> customRankingAttrs;
+}
+
+class Attribute {
+
+    private String name;
+    private Object value;
+}
+
+class Promotion {
+
+    private Doc doc;
+    private int indexInResult;
+}
