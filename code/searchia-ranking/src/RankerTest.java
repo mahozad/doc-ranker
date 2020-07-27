d17fdb6b43e7703a3497d622cd2b13781ee442c4
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2020-07-27 18:47:29 +0430

    Add some sample docs and use it in tests

--- code/searchia-ranking/src/RankerTest.java
+++ code/searchia-ranking/src/RankerTest.java
@@ -2,11 +2,18 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class RankerTest {
+
+    Path samplesPath = Path.of("src/sample-docs.txt");
 
     String query;
     List<Doc> matches;
@@ -14,34 +21,36 @@ class RankerTest {
     RankConfiguration configuration;
 
     @BeforeEach
-    void setUp() {
+    void setUp() throws IOException {
         query = "charger";
-        matches = List.of(
-                new Doc(2),
-                new Doc(2),
-                new Doc(2),
-                new Doc(2),
-                new Doc(2),
-                new Doc(2),
-                new Doc(2),
-                new Doc(2),
-                new Doc(2),
-                new Doc(2),
-                new Doc(2),
-                new Doc(2),
-                new Doc(2),
-                new Doc(2),
-                new Doc(2)
-        );
+
+        matches = Files
+                .lines(samplesPath)
+                .skip(1)
+                .map(line -> {
+                    String[] attrs = line.split("\\|");
+                    int id = Integer.parseInt(attrs[0].split("=")[1]);
+                    long creationDate = Long.parseLong(attrs[1].split("=")[1]);
+                    long viewCount = Long.parseLong(attrs[2].split("=")[1]);
+                    double score = Math.random();
+                    Attribute<String> title = new Attribute<>(attrs[3].split("=")[0], attrs[3].split("=")[0]);
+                    Attribute<String> description = new Attribute<>(attrs[4].split("=")[0], attrs[4].split("=")[0]);
+                    List<Attribute<String>> searchableAttrs = List.of(title, description);
+                    Map<String, Long> customAttrs = Map.of("viewCount", viewCount, "creationDate", creationDate);
+                    return new Doc(id, customAttrs, score, searchableAttrs);
+                })
+                .collect(Collectors.toList());
+
         promotions = List.of(
                 new Promotion(),
                 new Promotion()
         );
+
         configuration = new RankConfiguration(
                 "price",
                 null,
                 false,
-                List.of("views", "clicks")
+                List.of("viewCount", "creationDate")
         );
     }
 
