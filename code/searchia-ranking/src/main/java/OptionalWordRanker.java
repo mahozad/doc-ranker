3024652c96932f12dfe8a187b5d14b3ad53b6639
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2020-07-29 14:59:14 +0430

    Add initial impl. of OptionalWordRanker

---
+++ code/searchia-ranking/src/main/java/OptionalWordRanker.java
@@ -0,0 +1,55 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class OptionalWordRanker {
+
+    public static List<Doc> rankByOptionalWords(List<Doc> docs, RankConfiguration configuration) {
+        if (configuration.getQueryOptionalWords().isEmpty()) {
+            return docs;
+        }
+
+        docs = docs.subList(0, 10); // only top 10 docs
+        docs.forEach(doc -> doc.setPhaseScore(0)); // reset scores
+        SortedMap<Integer, List<Doc>> docsByPhaseScore = groupDocsByPhaseScore(docs);
+
+        List<Doc> result = new ArrayList<>();
+        docsByPhaseScore.keySet().forEach(key -> {
+            List<Doc> group = docsByPhaseScore.get(key);
+            for (Doc doc : group) {
+                for (String optionalWord : configuration.getQueryOptionalWords()) {
+                    if (isWordInDoc(optionalWord, doc)) {
+                        doc.setPhaseScore(doc.getPhaseScore() + 1);
+                    }
+                }
+            }
+            result.addAll(group.stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList()));
+        });
+
+        return result;
+    }
+
+    public static SortedMap<Integer, List<Doc>> groupDocsByPhaseScore(List<Doc> docs) {
+        Map<Integer, List<Doc>> map = docs.stream().collect(Collectors.groupingBy(Doc::getPhaseScore));
+        TreeMap<Integer, List<Doc>> sortedMap = new TreeMap<>(Comparator.reverseOrder());
+        sortedMap.putAll(map);
+        return sortedMap;
+    }
+
+    public static boolean isWordInDoc(String word, Doc doc) {
+        Set<String> docWords = getAllDocWords(doc);
+        for (String docWord : docWords) {
+            int distance = TypoRanker.measureWordsDistance(word, docWord, 2);
+            if (distance >= 0 && distance <= 2) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static Set<String> getAllDocWords(Doc doc) {
+        return doc.getSearchableAttrs()
+                .stream()
+                .flatMap(attribute -> Arrays.stream(TypoRanker.tokenizeText(attribute.getValue())))
+                .collect(Collectors.toSet());
+    }
+}
---
+++ code/searchia-ranking/src/test/java/OptionalWordRankerTest.java
@@ -0,0 +1,124 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.SortedMap;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OptionalWordRankerTest {
+
+    Path samplesPath = Path.of("src/test/resources/sample-docs.txt");
+
+    String query;
+    List<Doc> docs;
+    List<Promotion> promotions;
+    RankConfiguration configuration;
+
+    @BeforeEach
+    void setUp() throws IOException {
+        query = "dodge charger";
+
+        docs = Files
+                .lines(samplesPath)
+                .skip(1)
+                .map(line -> {
+                    String[] attrs = line.split("\\|");
+                    int id = Integer.parseInt(attrs[0].split("=")[1]);
+                    long creationDate = Long.parseLong(attrs[1].split("=")[1]);
+                    long viewCount = Long.parseLong(attrs[2].split("=")[1]);
+                    double score = Math.random();
+                    Attribute<String> title = new Attribute<>(attrs[3].split("=")[0], attrs[3].split("=")[1]);
+                    Attribute<String> description = new Attribute<>(attrs[4].split("=")[0], attrs[4].split("=")[1]);
+                    List<Attribute<String>> searchableAttrs = List.of(title, description);
+                    Map<String, Long> customAttrs = Map.of("viewCount", viewCount, "creationDate", creationDate);
+                    return new Doc(id, customAttrs, score, searchableAttrs);
+                })
+                .collect(Collectors.toList());
+
+        promotions = List.of(
+                new Promotion(),
+                new Promotion()
+        );
+
+        configuration = new RankConfiguration(
+                "price",
+                null,
+                false,
+                List.of("viewCount", "creationDate"),
+                Set.of("dodge")
+        );
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void rankByOptionalWords() {
+        List<Doc> result = OptionalWordRanker.rankByOptionalWords(docs, configuration);
+
+        assertEquals(1, result.get(0).getId());
+    }
+
+    @Test
+    void rankByOptionalWords_noOptionalWord() {
+        configuration = new RankConfiguration(
+                "price",
+                null,
+                false,
+                List.of("viewCount", "creationDate"),
+                Set.of()
+        );
+
+        List<Doc> result = OptionalWordRanker.rankByOptionalWords(docs, configuration);
+
+        assertEquals(docs, result);
+    }
+
+    @Test
+    void isWordInDoc() {
+        String word = "dodge";
+        Doc doc = docs.get(0);
+
+        boolean hasWord = OptionalWordRanker.isWordInDoc(word, doc);
+
+        assertTrue(hasWord);
+    }
+
+    @Test
+    void isWordInDoc_not() {
+        String word = "vision";
+        Doc doc = docs.get(0);
+
+        boolean hasWord = OptionalWordRanker.isWordInDoc(word, doc);
+
+        assertFalse(hasWord);
+    }
+
+    @Test
+    void getAllDocWords() {
+        Doc doc = docs.get(13);
+
+        Set<String> words = OptionalWordRanker.getAllDocWords(doc);
+
+        assertEquals(6, words.size());
+    }
+
+    @Test
+    void groupDocsByPhaseScore() {
+        docs.get(0).setPhaseScore(7);
+
+        SortedMap<Integer, List<Doc>> result = OptionalWordRanker.groupDocsByPhaseScore(docs);
+
+        assertEquals(7, result.firstKey());
+    }
+
+}
