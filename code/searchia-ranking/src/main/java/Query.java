2a8a492206c5e280c3eb6e62c284e4a287bae2ea
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2020-08-03 20:26:07 +0430

    Add Query class

---
+++ code/searchia-ranking/src/main/java/Query.java
@@ -0,0 +1,30 @@
+public class Query {
+
+    enum QueryType {
+        ORIGINAL, WILDCARD, CORRECTED, SUGGESTED, SPACED, EQUIVALENT, STEM, OPTIONAL
+    }
+
+    private String text;
+    private QueryType type;
+
+    public Query(String text, QueryType type) {
+        this.text = text;
+        this.type = type;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public QueryType getType() {
+        return type;
+    }
+
+    public void setType(QueryType type) {
+        this.type = type;
+    }
+}
