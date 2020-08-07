417ebd794e085ca4729d9c00beba8fde9e5e0123
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2020-08-07 16:10:24 +0430

    Remove and update some unused dependencies

--- code/searchia-ranking/build.gradle.kts
+++ code/searchia-ranking/build.gradle.kts
@@ -15,9 +15,6 @@ dependencies {
     implementation(files("lib/stemmer-complete-0.1.0.jar"))
     implementation(kotlin("stdlib-jdk8"))
     implementation("org.elasticsearch:elasticsearch:7.1.1")
-    implementation("org.junit.platform:junit-platform-commons:1.6.2")
-    implementation("org.opentest4j:opentest4j:1.2.0")
-    implementation("org.apiguardian:apiguardian-api:1.1.0")
 
     testImplementation("org.hamcrest:hamcrest:2.2")
     testImplementation("org.junit.jupiter:junit-jupiter:5.6.2")
@@ -25,6 +22,7 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-engine:5.6.2")
     testImplementation("org.junit.jupiter:junit-jupiter-params:5.6.2")
     testImplementation("org.junit.platform:junit-platform-engine:1.6.2")
+    testImplementation("org.junit.platform:junit-platform-commons:1.6.2")
 }
 
 configure<JavaPluginConvention> {
