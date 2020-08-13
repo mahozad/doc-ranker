d0713e14d1a56cbfb0dc3fa4d3430f8f654fe3e0
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2020-08-13 14:50:49 +0430

    Rename RankerPhase interface to Ranker

--- code/searchia-ranking/src/main/java/ir/parsijoo/searchia/Ranker.java
+++ code/searchia-ranking/src/main/java/ir/parsijoo/searchia/Ranker.java
@@ -3,7 +3,7 @@ package ir.parsijoo.searchia;
 import java.util.List;
 import java.util.Map;
 
-public interface RankerPhase {
+public interface Ranker {
 
     void rank(Map<Query.QueryType, Query> queries, List<Doc> docs);
 }
