e5a00cff86a2ad9f05a7161a65fdb4f07a444b93
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2020-08-13 20:28:55 +0430

    Make the fields final and add accessors for them

--- code/searchia-ranking/src/main/java/ir/parsijoo/searchia/dto/RankingPhaseDTO.java
+++ code/searchia-ranking/src/main/java/ir/parsijoo/searchia/dto/RankingPhaseDTO.java
@@ -4,15 +4,31 @@ import java.io.Serializable;
 
 public class RankingPhaseDTO implements Serializable {
 
-    private RankingPhaseType type;
-    private boolean enabled;
-    private int order;
-    private SortDirection sortDirection;
+    private final RankingPhaseType type;
+    private final boolean enabled;
+    private final int order;
+    private final SortDirection sortDirection;
 
     public RankingPhaseDTO(RankingPhaseType type, boolean enabled, int order, SortDirection sortDirection) {
         this.order = order;
         this.type = type;
         this.enabled = enabled;
         this.sortDirection = sortDirection;
+    }
+
+    public RankingPhaseType getType() {
+        return type;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public int getOrder() {
+        return order;
+    }
+
+    public SortDirection getSortDirection() {
+        return sortDirection;
     }
 }
