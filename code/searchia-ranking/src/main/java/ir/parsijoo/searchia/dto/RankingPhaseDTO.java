8e1250aafd065e5b90217aeff4fdd2c370ba0d21
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2020-08-13 20:58:11 +0430

    Remove CustomRankingPhaseDTO and merge its fields in the super class

--- code/searchia-ranking/src/main/java/ir/parsijoo/searchia/dto/RankingPhaseDTO.java
+++ code/searchia-ranking/src/main/java/ir/parsijoo/searchia/dto/RankingPhaseDTO.java
@@ -8,12 +8,17 @@ public class RankingPhaseDTO implements Serializable {
     private final boolean enabled;
     private final int order;
     private final SortDirection sortDirection;
+    /**
+     * Name of the attribute used for custom phases. Example: "viewCount"
+     */
+    private final String attributeName;
 
-    public RankingPhaseDTO(RankingPhaseType type, boolean enabled, int order, SortDirection sortDirection) {
+    public RankingPhaseDTO(RankingPhaseType type, boolean enabled, int order, SortDirection sortDirection, String attributeName) {
         this.order = order;
         this.type = type;
         this.enabled = enabled;
         this.sortDirection = sortDirection;
+        this.attributeName = attributeName;
     }
 
     public RankingPhaseType getType() {
@@ -30,5 +35,9 @@ public class RankingPhaseDTO implements Serializable {
 
     public SortDirection getSortDirection() {
         return sortDirection;
+    }
+
+    public String getAttributeName() {
+        return attributeName;
     }
 }
