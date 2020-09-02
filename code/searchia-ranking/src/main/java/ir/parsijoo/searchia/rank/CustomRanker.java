ff173836c95ad8c3c50bbfc2815f11f81c6f5438
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2020-09-02 15:17:23 +0430

    Update CustomRanker to accept Number type instead of Double

--- code/searchia-ranking/src/main/java/ir/parsijoo/searchia/rank/CustomRanker.java
+++ code/searchia-ranking/src/main/java/ir/parsijoo/searchia/rank/CustomRanker.java
@@ -15,11 +15,11 @@ public class CustomRanker implements Ranker {
     public void rank(Map<QueryType, Query> queries, List<Record> records, RankingPhase phase) {
         String attributeName = phase.getAttributeName();
         Object attr = records.get(0).getCustomRankingAttrs().get(attributeName);
-        if (attr instanceof Boolean) {
-            Selector<Boolean> selector = record -> (Boolean) record.getCustomRankingAttrs().get(attributeName);
+        if (attr instanceof Number) {
+            Selector<Double> selector = record -> ((Number) record.getCustomRankingAttrs().get(attributeName)).doubleValue();
             new RankUpdater<>(records, selector, phase.getSortDirection()).updateRanks();
-        } else if (attr instanceof Double) {
-            Selector<Double> selector = record -> (Double) record.getCustomRankingAttrs().get(attributeName);
+        } else if (attr instanceof Boolean) {
+            Selector<Boolean> selector = record -> (Boolean) record.getCustomRankingAttrs().get(attributeName);
             new RankUpdater<>(records, selector, phase.getSortDirection()).updateRanks();
         } else {
             throw new UnsupportedAttributeTypeException(attributeName);
@@ -28,7 +28,7 @@ public class CustomRanker implements Ranker {
 
     public static class UnsupportedAttributeTypeException extends RuntimeException {
         public UnsupportedAttributeTypeException(String attributeName) {
-            super(String.format("The attribute \"%s\" is not of type Boolean or Double", attributeName));
+            super(String.format("The attribute \"%s\" is not numeric or boolean", attributeName));
         }
     }
 }
