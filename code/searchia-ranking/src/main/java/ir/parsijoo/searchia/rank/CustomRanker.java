5f701950daec96da142b756b7df9d0971048b175
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2020-09-04 01:17:43 +0430

    Generify the attribute type in CustomRanker to simplify the code

--- code/searchia-ranking/src/main/java/ir/parsijoo/searchia/rank/CustomRanker.java
+++ code/searchia-ranking/src/main/java/ir/parsijoo/searchia/rank/CustomRanker.java
@@ -14,21 +14,7 @@ public class CustomRanker implements Ranker {
     @Override
     public void rank(Map<QueryType, Query> queries, List<Record> records, RankingPhase phase) {
         String attributeName = phase.getAttributeName();
-        Object attr = records.get(0).getCustomRankingAttrs().get(attributeName);
-        if (attr instanceof Number) {
-            Selector<Double> selector = record -> ((Number) record.getCustomRankingAttrs().get(attributeName)).doubleValue();
-            new RankUpdater<>(records, selector, phase.getSortDirection()).updateRanks();
-        } else if (attr instanceof Boolean) {
-            Selector<Boolean> selector = record -> (Boolean) record.getCustomRankingAttrs().get(attributeName);
-            new RankUpdater<>(records, selector, phase.getSortDirection()).updateRanks();
-        } else {
-            throw new UnsupportedAttributeTypeException(attributeName);
-        }
-    }
-
-    public static class UnsupportedAttributeTypeException extends RuntimeException {
-        public UnsupportedAttributeTypeException(String attributeName) {
-            super(String.format("The attribute \"%s\" is not numeric or boolean", attributeName));
-        }
+        Selector<Comparable> selector = record -> record.getCustomRankingAttrs().get(attributeName);
+        new RankUpdater<>(records, selector, phase.getSortDirection()).updateRanks();
     }
 }
